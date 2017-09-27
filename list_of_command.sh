328db5e0934fe5a0873080938b9ae35673e2c25e
abubakr123 <abubakryagob@gmail.com>
2017-09-27 01:20:56 +0000

    Modify the list_of_command.sh and handell it

--- list_of_command.sh
+++ list_of_command.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-#check if the number of the files is equal
-ls -1 *_122_133.ar | wc -l
-ls -1 *_122_255.ar | wc -l
-ls -1 *_122_377.ar | wc -l
-ls -1 *_122_499.ar | wc -l
 
-#creat a file list
-#ls -1 *_122_133.ar > list1.txt
-
-awk '{print substr($0, 1, length($0)-11)}' list1.txt |
+#creat a list of command lines 
+ls -1 *_122_133.ar | awk '{print substr($0, 1, length($0)-11)}'|
 while read -r; do
    echo "python2.7 reduceSingleStation_modefy.py  --stem  "$REPLY"  --indir  --outdir  --eph  --tscr  --fscr  --psrsh  --clean  --verbose">>list_of_command.txt
 
 done
+
+
