191dce27c74c74199a2fcb928f54af36f83ed7fa
abubakr123 <abubakryagob@gmail.com>
2017-09-30 15:34:44 +0000

    Added path directory as an argument to the script

--- list_of_command.sh
+++ list_of_command.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+path="$1"
 
 #creat a list of command lines 
-ls -1 *_122_133.ar | awk '{print substr($0, 1, length($0)-11)}'|
+echo "The path directory " $path
+ls -1 $path/*_122_133.ar | awk -F "/|_" '{print $9 $10 $11 }'|
+
 while read -r; do
-   echo "python2.7 reduceSingleStation_modefy.py  --stem  "$REPLY"  --indir  --outdir  --eph  --tscr  --fscr  --psrsh  --clean  --verbose">>list_of_command.txt
+  echo "python2.7 /home/abubakr/MSc_Project/reduce_single_station.py --stem "$REPLY" --indir /data0/abubakr/B0138+59/raw --outdir /data0/abubakr/B0138+59/result --tscr --fscr --psrsh --clean --verbose">>list_of_command.txt
 
 done
+
 
 
