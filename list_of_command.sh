c61996dbdb943c04cfe6e09036d5c4255bcd9502
abubakr123 <abubakryagob@gmail.com>
2017-10-03 08:32:04 +0000

    this perform for data0 ... path

--- list_of_command.sh
+++ list_of_command.sh
@@ -4,10 +4,10 @@ path="$1"
 
 #creat a list of command lines 
 echo "The path directory " $path
-ls -1 $path/*_122_133.ar | awk -F "/|_" '{print $9 $10 $11 }'|
+ls -1 $path/*_122_133.ar | awk -F "/|_" '{print $6 $7 "_" $8  }'|
 
 while read -r; do
-  echo "python2.7 /home/abubakr/MSc_Project/reduce_single_station.py --stem "$REPLY" --indir /data0/abubakr/B0138+59/raw --outdir /data0/abubakr/B0138+59/result --tscr --fscr --psrsh --clean --verbose">>list_of_command.txt
+  echo "python2.7 /home/abubakr/MSc_Project/reduce_single_station.py --stem "$REPLY" --indir /data0/abubakr/B1133+16/raw --outdir /data0/abubakr/B1133+16/result --tscr --fscr --psrsh --clean --verbose">>list_of_command.txt
 
 done
 
