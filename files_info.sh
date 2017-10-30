61c6bbc2a40d1dc9d700b0ce151bc1254979e413
abubakr123 <abubakryagob@gmail.com>
2017-10-30 01:26:27 +0000

    Added a new version of files_info.sh compelate for table info and the plots

--- files_info.sh
+++ files_info.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+
 #pass the directory of the  main latex sript (header_plots.tex)
 path="/home/abubakr/MSc_Project"
 
@@ -12,10 +14,11 @@ echo $stem_name
 
 #creat a loop through the files with different exctensions
 for f in $stem_name ; do
-	#stem=`psredit -c file $f`
-	sed  "s/xstem/$f/g" $path/header_plots.tex > "${f}"_header_plots2.tex
+	sed  "s/xstem/$f/g" $path/header_plots.tex >> "${f}"_header_plots2.tex
+	sed -i 's/_/\\_/g' "${f}"_header_plots2.tex
 	name=`psredit -Q -q -c name $f.ar.pscr.zap.F`
 	sed -i "s/xname/$name/g" "${f}"_header_plots2.tex
+	#sed -i 's/+/\$+$/g' "${f}"_header_plots2.tex
 	nbin=`psredit -Q -q -c nbin $f.ar.pscr.zap.F`
 	sed -i "s/xnbin/$nbin/g" "${f}"_header_plots2.tex
 	npol=`psredit -Q -q -c npol $f.ar.pscr.zap.F`
@@ -43,12 +46,42 @@ for f in $stem_name ; do
 	snr=`psrstat -Q -q -j tscrunch -c snr $f.ar.pscr.zap.F`
 	sed -i "s/xsnr/$snr/g" "${f}"_header_plots2.tex 
 	rfi_sum=`sed '1,4d' $f.psh | wc -l`
-	RFI_percent=$(( $((nsubint*nchan))*100   | bc -l ))
+	RFI_percent=$(echo "($rfi_sum/$nsubint * $nchan) * 100" | bc -l) 
 	sed -i "s/xRFI/$RFI_percent/g" "${f}"_header_plots2.tex
-
 	echo $rfi_sum 
 	echo $RFI_percent
 	echo $nchan
 	echo $nsubint
+
+
+	fprof_plot=`psrplot -D "${f}_fprof.ps/cps" -p freq+ -j 'tscrunch' -j 'dedisperse' $f.ar.pscr.zap`
+	ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
+	rm ${f}_fprof.ps
+	sed -i "s/xfprof/${f}_fprof.pdf/g" "${f}"_header_plots2.tex
+
+	ds_plot=`pav -g "${f}_ds.ps/cps" -j $f.ar.pscr.zap` 
+	ps2pdf ${f}_ds.ps ${f}_ds.pdf
+	rm ${f}_ds.ps
+	sed -i "s/xds/${f}_ds.pdf/g" "${f}"_header_plots2.tex
+
+	bp_plot=`pav -g "${f}_bp.ps/cps" -J $f.ar.pscr.zap.T`
+	ps2pdf ${f}_bp.ps ${f}_bp.pdf
+	rm ${f}_bp.ps
+	sed -i "s/xbp/${f}_bp.pdf/g" "${f}"_header_plots2.tex
+
+	avprof_plot=`pav -g "${f}_avprof.ps/cps" -DTd $f.ar.pscr.zap.F`
+	ps2pdf ${f}_avprof.ps ${f}_avprof.pdf
+	rm ${f}_avprof.ps 
+	sed -i "s/xavprof/${f}_avprof.pdf/g"  "${f}"_header_plots2.tex
+
+	stack_plot=`pav -g "${f}_stack.ps/cps" -Yd $f.ar.pscr.zap.F`
+	ps2pdf ${f}_stack.ps ${f}_stack.pdf
+	rm ${f}_stack.ps
+	sed -i "s/xstack/${f}_stack.pdf/g" "${f}"_header_plots2.tex
+
+	pdflatex ${f}_header_plots2.tex
+	#ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
+	#ps2pdf ${f}_ds.ps ${f}_ds.pdf
+
 done
 
