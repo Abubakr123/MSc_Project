854658ecc52cc48b05b6e4c66298922a020a0d8f
abubakr123 <abubakryagob@gmail.com>
2017-10-31 09:32:11 +0000

    Added the final version of the files_info.sh

--- files_info.sh
+++ files_info.sh
@@ -14,11 +14,12 @@ echo $stem_name
 
 #creat a loop through the files with different exctensions
 for f in $stem_name ; do
+	echo "Start processing the table informations"
 	sed  "s/xstem/$f/g" $path/header_plots.tex >> "${f}"_header_plots2.tex
 	sed -i 's/_/\\_/g' "${f}"_header_plots2.tex
 	name=`psredit -Q -q -c name $f.ar.pscr.zap.F`
 	sed -i "s/xname/$name/g" "${f}"_header_plots2.tex
-	#sed -i 's/+/\$+$/g' "${f}"_header_plots2.tex
+	sed -i 's/+/\$+$/g' "${f}"_header_plots2.tex
 	nbin=`psredit -Q -q -c nbin $f.ar.pscr.zap.F`
 	sed -i "s/xnbin/$nbin/g" "${f}"_header_plots2.tex
 	npol=`psredit -Q -q -c npol $f.ar.pscr.zap.F`
@@ -46,13 +47,10 @@ for f in $stem_name ; do
 	snr=`psrstat -Q -q -j tscrunch -c snr $f.ar.pscr.zap.F`
 	sed -i "s/xsnr/$snr/g" "${f}"_header_plots2.tex 
 	rfi_sum=`sed '1,4d' $f.psh | wc -l`
-	RFI_percent=$(echo "($rfi_sum/$nsubint * $nchan) * 100" | bc -l) 
+	RFI_percent=$(echo "($rfi_sum/($nsubint * $nchan)) * 100" | bc -l) 
 	sed -i "s/xRFI/$RFI_percent/g" "${f}"_header_plots2.tex
-	echo $rfi_sum 
-	echo $RFI_percent
-	echo $nchan
-	echo $nsubint
 
+	echo "Table infomation updated. Start producing the plots and convert them to pdf files."
 
 	fprof_plot=`psrplot -D "${f}_fprof.ps/cps" -p freq+ -j 'tscrunch' -j 'dedisperse' $f.ar.pscr.zap`
 	ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
@@ -79,9 +77,12 @@ for f in $stem_name ; do
 	rm ${f}_stack.ps
 	sed -i "s/xstack/${f}_stack.pdf/g" "${f}"_header_plots2.tex
 
+	echo "Run pdflatex to get pdf version of the the latex template."
 	pdflatex ${f}_header_plots2.tex
-	#ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
-	#ps2pdf ${f}_ds.ps ${f}_ds.pdf
 
 done
 
+echo "Latex template done. Producing final combined pdf file."
+pdfjoin *_header_plots2.pdf --a4paper --no-landscape  -o combined_files.pdf --rotateoversize false
+
+echo "End of Processing"
