79dac8be9891bdbf92a4a0c53db801e34291e7b0
abubakr123 <abubakryagob@gmail.com>
2018-04-16 12:39:43 +0000

    update files_info_GLOW.sh

--- files_info_GLOW.sh
+++ files_info_GLOW.sh
@@ -6,7 +6,7 @@
 path="/home/abubakr/MSc_Project"
 
 #prepare a list of the files
-file_list="*.ar.pscr.zap.F"
+file_list="*.pscr.zap.F"
 
 #prepare and check the stem name (e.g B1122+25_D20140125T124533)
 stem_name=`ls -1 *$file_list | awk -F '.'  '{print $1 "." $2}'`
@@ -14,8 +14,8 @@ echo $stem_name
 
 #creat a loop through the files with different exctensions
 for f in $stem_name ; do
-	echo "Start processing the table informations"
-	sed  "s/xstem/$f/g" $path/header_plots.tex >> "${f}"_header_plots2.tex
+	echo -e "\nStart processing the table informations\n"
+	sed  "s/xstem/$f/g" $path/header_plots_GLOW.tex >> "${f}"_header_plots2.tex
 	sed -i 's/_/\\_/g' "${f}"_header_plots2.tex
 	name=`psredit -Q -q -c name $f.ar.pscr.zap.F`
 	sed -i "s/xname/$name/g" "${f}"_header_plots2.tex
@@ -50,39 +50,39 @@ for f in $stem_name ; do
 	RFI_percent=$(echo "($rfi_sum/($nsubint * $nchan)) * 100" | bc -l) 
 	sed -i "s/xRFI/$RFI_percent/g" "${f}"_header_plots2.tex
 
-	echo "Table infomation updated. Start producing the plots and convert them to pdf files."
+	echo -e "\nTable infomation updated. Start producing the plots and convert them to pdf files.\n"
 
 	fprof_plot=`psrplot -D "${f}_fprof.ps/cps" -p freq+ -j 'tscrunch' -j 'dedisperse' $f.ar.pscr.zap`
-#	ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
-#	rm ${f}_fprof.ps
-#	sed -i "s/xfprof/${f}_fprof.pdf/g" "${f}"_header_plots2.tex
+	ps2pdf ${f}_fprof.ps ${f}_fprof.pdf
+	rm ${f}_fprof.ps
+	sed -i "s/xfprof/${f}_fprof.pdf/g" "${f}"_header_plots2.tex
 
 	ds_plot=`pav -g "${f}_ds.ps/cps" -j $f.ar.pscr.zap` 
-#	ps2pdf ${f}_ds.ps ${f}_ds.pdf
-#	rm ${f}_ds.ps
-#	sed -i "s/xds/${f}_ds.pdf/g" "${f}"_header_plots2.tex
+	ps2pdf ${f}_ds.ps ${f}_ds.pdf
+	rm ${f}_ds.ps
+	sed -i "s/xds/${f}_ds.pdf/g" "${f}"_header_plots2.tex
 
 	bp_plot=`pav -g "${f}_bp.ps/cps" -J $f.ar.pscr.zap.T`
-#	ps2pdf ${f}_bp.ps ${f}_bp.pdf
-#	rm ${f}_bp.ps
-#	sed -i "s/xbp/${f}_bp.pdf/g" "${f}"_header_plots2.tex
+	ps2pdf ${f}_bp.ps ${f}_bp.pdf
+	rm ${f}_bp.ps
+	sed -i "s/xbp/${f}_bp.pdf/g" "${f}"_header_plots2.tex
 
 	avprof_plot=`pav -g "${f}_avprof.ps/cps" -DTd $f.ar.pscr.zap.F`
-#	ps2pdf ${f}_avprof.ps ${f}_avprof.pdf
-#	rm ${f}_avprof.ps 
-#	sed -i "s/xavprof/${f}_avprof.pdf/g"  "${f}"_header_plots2.tex
+	ps2pdf ${f}_avprof.ps ${f}_avprof.pdf
+	rm ${f}_avprof.ps 
+	sed -i "s/xavprof/${f}_avprof.pdf/g"  "${f}"_header_plots2.tex
 
 	stack_plot=`pav -g "${f}_stack.ps/cps" -Yd $f.ar.pscr.zap.F`
-#	ps2pdf ${f}_stack.ps ${f}_stack.pdf
-#	rm ${f}_stack.ps
-#	sed -i "s/xstack/${f}_stack.pdf/g" "${f}"_header_plots2.tex
+	ps2pdf ${f}_stack.ps ${f}_stack.pdf
+	rm ${f}_stack.ps
+	sed -i "s/xstack/${f}_stack.pdf/g" "${f}"_header_plots2.tex
 
-#	echo "Run pdflatex to get pdf version of the the latex template."
-#	pdflatex ${f}_header_plots2.tex
+	echo "Run pdflatex to get pdf version of the the latex template."
+	pdflatex ${f}_header_plots2.tex
 
 done
 
-echo "Latex template done. Producing final combined pdf file."
-#pdfjoin *_header_plots2.pdf --a4paper --no-landscape  -o combined_files.pdf --rotateoversize false
+echo -e "\nLatex template done. Producing final combined pdf file.\n"
+pdfjoin *_header_plots2.pdf --a4paper --no-landscape  -o combined_files.pdf --rotateoversize false
 
-echo "End of Processing"
+echo -e "\nEnd of Processing"
